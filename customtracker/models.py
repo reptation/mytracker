3d2daac4445cef6e104d75cbddaeff65a0afe459
reptation <david [at] musicalquilt dot com>
2018-05-16 04:01:56 +0000

    notes

--- customtracker/models.py
+++ customtracker/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+#from django.contrib.auth.models import authenticate
 
-# Create your models here.
+
+# need users, items, measure words
+#user = User.objects.create_user('john', 'lennon@thebeatles.com', 'johnpassword')
+#user = User.objects.create_user('john', 'secret')
+#user.save()
+
+# tables
+# ---------------
+# username, 
+# item_names 
+# measure_words
+
+# session =>
+# item, date, "level" (aka length or amount depending on measure word), username
