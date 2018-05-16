7249a76629ab3943ca71884eca757d4b23cc6f0d
reptation <david [at] musicalquilt dot com>
2018-05-16 04:47:53 +0000

    added to models. discovered through statement

--- customtracker/models.py
+++ customtracker/models.py
@@ -13,6 +13,36 @@ from django.contrib.auth.models import User
 # username, 
 # item_names 
 # measure_words
+Class AppUser(models.Model):
+    username = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.CharField(max_length=100)
+    #this should be done by base class
+    #password = 
+    # one to many?
+    items = models.ManyToManyField(Item)
+    preferred_mw = models.ManyToManyField(MeasureWord, through='Pairings')
+    # preferred measure words? fk lookup on items
+
+Class Item(model.Model):
+    name = models.CharField(max_length=100)
+
+Class MeasureWord(model.Model):
+    name = models.CharField(max_length=100)
+
+Class Pairings:
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    measure_word = models.ForeignKey(MeasureWord, on_delete=models.CASCADE)
+
+Class Session(model.Model):
+    # date
+    # username fk
+    # item fk
+    # amount
+    # measure word fk 
+
+#measure words 
 
 # session =>
 # item, date, "level" (aka length or amount depending on measure word), username
